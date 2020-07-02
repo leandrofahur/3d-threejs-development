d5ff9b7e78179576b9e8867f83c3353f72992874
leandrofahur <leandrofahur@gmail.com>
2020-07-02 01:48:40 -0700

    simple program

---
+++ threejs_sandbox/index.js
@@ -0,0 +1,38 @@
+class World {
+  constructor() {
+
+    this.t = 0.0;
+
+    // Set the scene:
+    this.scene = new THREE.Scene();
+
+    // Set the camera:
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    // Set the renderer:
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    const geometry = new THREE.CircleGeometry(0.1,360);
+    const material = new THREE.MeshBasicMaterial({color: 0xcccccc});
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    this.camera.position.z = 5;
+    this.animate();
+  }
+
+  animate() {
+    const game = this;
+    requestAnimationFrame(()=>{
+      game.animate();
+    });
+
+    this.cube.position.x = Math.sin(this.t);
+    this.cube.position.y = Math.cos(this.t);
+    this.t += 2*Math.PI/360;
+
+	  this.renderer.render(this.scene, this.camera);
+  }
+}
